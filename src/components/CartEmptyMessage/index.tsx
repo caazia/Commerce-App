2f4304cdb785d34c563b81e7978378d7f539e0e5
caazia <contato.cassia.carvalho@gmail.com>
2025-02-10 21:19:44 -0300

    feat: ajuste de componentes header e CartPage.

---
+++ src/components/CartEmptyMessage/index.tsx
@@ -0,0 +1,12 @@
+import Typography from "../Typography"
+
+const CartEmptyMessage = () => {
+
+  return (
+  <div style={{ marginTop: "15px" }}>
+    <Typography> Não existem produtos no carrinho. </Typography>
+  </div>
+  )
+}
+
+export default CartEmptyMessage;
